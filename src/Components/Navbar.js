b888b8e0bf798741b75c505c95cae439096a0fe6
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-12-27 19:10:55 +0600

    added navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
+  return (
+    <div>
+      <nav
+        x-data="{ isOpen: false }"
+        className="relative bg-white shadow-md dark:bg-gray-800"
+      >
+        <div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+          <div className="flex items-center justify-between">
+            <div>
+              <a
+                className="text-2xl font-bold text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300"
+                href="/"
+              >
+                RFQ TEAM
+              </a>
+            </div>
+
+            {/* <!-- Mobile menu button --> */}
+            <div className="flex lg:hidden">
+              <button
+                type="button"
+                className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                aria-label="toggle menu"
+                
+              >
+                {
+                  isOpen ||  <svg
+                  onClick={() => setIsOpen(true)}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 8h16M4 16h16"
+                  />
+                </svg>
+                }
+
+                {
+                  isOpen && <svg
+                  onClick={() => setIsOpen(false)}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+                }
+              </button>
+            </div>
+          </div>
+
+          {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
+          
+            <div className="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center">
+              <div className="flex flex-col md:flex-row md:mx-6">
+                <a
+                  className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+                  href="/"
+                >
+                  About
+                </a>
+                <a
+                  className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+                  href="/"
+                >
+                  Policy
+                </a>
+                <a
+                  className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+                  href="/"
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+
+          
+           
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
